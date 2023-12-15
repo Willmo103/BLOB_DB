40ea9cfb25ef6a5d59990e38a5a218654168662b
Will Morris <willmorris103@gmail.com>
2023-12-14 23:48:55 -0600

    are you there code? its me, refactoring

--- src/app/app.py
+++ src/app/app.py
@@ -1,46 +1,75 @@
-from flask import Flask, request, send_file, render_template, redirect, url_for
-import psycopg2, io
+import io
 from os import getenv
+import psycopg2
 from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, send_file, url_for
+
 load_dotenv()
 
-app = Flask(__name__)
-## set the jinja template config
+app = Flask(__name__, template_folder="../templates")
+
 
 def get_db_connection():
-    return psycopg2.connect(getenv('DATABASE_URL'))
+    return psycopg2.connect(getenv("DATABASE_URL"))
 
-@app.route('/')
+
+@app.route("/")
 def index():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT id, filename FROM scripts')
+    cur.execute("SELECT id, filename FROM scripts")
     files = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('index.html', files=files)
+    return render_template("index.html", files=files)
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def upload_file():
-    file = request.files['file']
+    file = request.files["file"]
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('INSERT INTO scripts (filename, data, mimetype) VALUES (%s, %s, %s)',
-                (file.filename, file.read(), file.mimetype))
+    cur.execute(
+        "INSERT INTO scripts (filename, data, mimetype) VALUES (%s, %s, %s)",
+        (file.filename, file.read(), file.mimetype),
+    )
     conn.commit()
     cur.close()
     conn.close()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/download/<int:id>')
+
+@app.route("/download/<int:id>")
 def download_file(id):
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT filename, data, mimetype FROM scripts WHERE id = %s', (id,))
+    cur.execute("SELECT filename, data, mimetype FROM scripts WHERE id = %s", (id,))
     file = cur.fetchone()
     cur.close()
     conn.close()
-    return send_file(io.BytesIO(file[1]), mimetype=file[2], as_attachment=True, attachment_filename=file[0])
+    if file:
+        return send_file(
+            io.BytesIO(file[1]),
+            mimetype=file[2],
+            as_attachment=True,
+            download_name=file[0],
+        )
+    else:
+        # Handle the case where the file is not found
+        return "File not found", 404
 
-if __name__ == '__main__':
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_file(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    # Delete the file entry from the database
+    cur.execute('DELETE FROM scripts WHERE id = %s', (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    # Redirect back to the homepage after deletion
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
